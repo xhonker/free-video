ecf2418d5c9b63347c9eeedeebe94b68509b4e73
quicker <892388878@qq.com>
2020-06-18 15:18:59 +0800

    init

---
+++ free-video.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/xhonker/free-video
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  // Your code here...
+})();
