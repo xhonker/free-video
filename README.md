629c14d5d9c1033c5614d35e4dcd4874e65cef1a
quicker <892388878@qq.com>
2020-06-19 16:21:09 +0800

    feat:tencent

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-#### HOW
+#### 怎么使用
+
+1、[下载 Tampermonkey 插件](./assets/Tampermonkey_4.1.0.crx)
+
+2、[安装脚本](https://xhonker.github.io/free-video/index.user.js)
---
+++ index.user.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         free-video
+// @namespace    https://github.com/xhonker/free-video
+// @version      0.1
+// @description  free play all china video
+// @supportURL   https://github.com/xhonker/free-video/issue
+// @updateURL    https://xhonker.gihub.io/free-video/index.user.js
+// @downloadURL  https://xhonker.gihub.io/free-video/index.user.js
+// @author       Quicker
+// @include       *://v.qq.com/x/cover/*
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/1.7.1/jquery.min.js
+// @grant        none
+// @run-at document-end
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  let baseURL = 'https://jx.618g.com/?url=';
+
+  loadTencent();
+
+  function loadTencent() {
+    let playerContainer = document.querySelector('#mod_player');
+    let tencentPlayer = document.querySelector('#tenvideo_player');
+    let vip = document.querySelector('#_vip_player_sec');
+    if (vip) vip.hidden = true;
+
+    let episode = document.querySelector('.mod_episode');
+    if (episode) {
+      episode.addEventListener('click', function (e) {
+        let event = e.target;
+        if (event.nodeName === 'A') {
+          episode.querySelector('.current').classList.toggle('current');
+          event.parentElement.classList.add('current');
+          loadIframe(event.href);
+        }
+      });
+    }
+    removeTxPlayer();
+
+    function loadIframe(href) {
+      let isLoad = document.querySelector('#mod_player iframe');
+
+      if (isLoad) playerContainer.removeChild(document.querySelector('iframe'));
+      let iframe = document.createElement('iframe');
+      iframe.src = `${baseURL}${href}`;
+      iframe.width = '100%';
+      iframe.height = '100%';
+      iframe.allowFullscreen = true;
+      playerContainer.appendChild(iframe);
+    }
+
+    function removeTxPlayer() {
+      let timer = setInterval(() => {
+        if (typeof window.TxpCreativePlayer !== 'undefined') {
+          TxpCreativePlayer = void 0;
+          playerContainer.removeChild(tencentPlayer);
+          tencentPlayer.innerHTML = '<span></span>';
+          tencentPlayer.hidden = true;
+          loadIframe(window.location.href);
+          clearInterval(timer);
+        }
+      }, 200);
+    }
+  }
+})();
