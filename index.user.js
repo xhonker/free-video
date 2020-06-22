34ef4d3fba1f37411b33a0d250c36ea9d55f8c8d
quicker <892388878@qq.com>
2020-06-22 09:52:58 +0800

    fix:support url

--- index.user.js
+++ index.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         free-video
 // @namespace    https://github.com/xhonker/free-video
-// @version      0.1
+// @version      0.1.1
 // @description  free play all china video
-// @supportURL   https://github.com/xhonker/free-video/issue
+// @supportURL   https://github.com/xhonker/free-video/issues
 // @updateURL    https://xhonker.gihub.io/free-video/index.user.js
 // @downloadURL  https://xhonker.gihub.io/free-video/index.user.js
 // @author       Quicker
